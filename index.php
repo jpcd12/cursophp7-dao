626bf131a9b5dbb4c5110ebae1b5484ea7950cf3
Joao Pedro Duarte <joaopedron@gmail.com>
2021-09-01 20:18:59 -0300

    Inserir e alterar

--- index.php
+++ index.php
@@ -16,8 +16,20 @@ require_once("config.php");
 //echo json_encode($search);
 
 //Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$");
+//echo $usuario;
+
+//Criando um novo usuário
+//$aluno = new Usuario("aluno", "@lun0");
+//$aluno->insert();
+//echo $aluno;
+
 $usuario = new Usuario();
-$usuario->login("root", "!@#$");
+
+$usuario->loadById(7);
+
+$usuario->update("Professor", "!@#!");
 
 echo $usuario;
 
